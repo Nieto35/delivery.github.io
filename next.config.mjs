bc638adc1f9932ba5c99afd3645fe8f0265d1e75
Nieto35 <jfnieto00@misena.edu.co>
2024-04-01 12:35:53 +0200

    changue output

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
-import path from 'path'
-
 export default {
   webpack: (config, { isServer }) => {
+    // Agregar la propiedad output a la configuración
+    config.output = {
+      // Aquí puedes especificar las opciones de output
+      // Por ejemplo, puedes especificar el directorio de salida y el nombre del archivo
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js'
+    }
+
     config.module.rules.push({
       test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
       use: [
